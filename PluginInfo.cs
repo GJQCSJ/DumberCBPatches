fcb4568c97a2b1a621759730bcd51e2f8c4b4a4d
GJQCSJ <a1480104746@163.com>
2025-05-28 03:23:10 +1000

    Separated config files

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -10,5 +10,5 @@ public static class PluginInfo
 
     public const string PLUGIN_NAME = "DumberCBPatches";
 
-    public const string PLUGIN_VERSION = "1.0.5";
+    public const string PLUGIN_VERSION = "1.0.6";
 }
